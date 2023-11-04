e6a21ab13ea755af0cb219de400a4ae4f4febc7d
cole-adams <colepadams99@gmail.com>
2023-11-03 20:38:45 -0600

    Reduce size of H3

--- app/common/components/Text/H3.tsx
+++ app/common/components/Text/H3.tsx
@@ -6,6 +6,6 @@ interface Props {
 
 export default function H3({ className, children }: PropsWithChildren<Props>) {
   return (
-    <h2 className={`${className ?? ""} md:text-4xl text-3xl`}>{children}</h2>
+    <h2 className={`${className ?? ""} md:text-3xl text-2xl`}>{children}</h2>
   );
 }
