def33d43e2f2e9afe1fec6ff0bd3220c82a5c366
cole-adams <colepadams99@gmail.com>
2023-11-07 00:45:38 -0700

    Add nav link animation

--- app/common/components/Navigation/NavLink.tsx
+++ app/common/components/Navigation/NavLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 import { LinkEntry } from "../../models/contentful";
 import AppLink from "../Link";
 
@@ -12,14 +13,19 @@ export default function NavLink({ entry }: Props) {
   const pathname = usePathname();
 
   return (
-    <AppLink
-      entry={entry}
-      className={`flex flex-col md:flex-row items-center gap-2 text-primary text-xl md:text-3xl md:pl-16 py-3 underline hover:bg-background ${
-        pathname === entry.fields.url
-          ? "md:bg-background border-b-4 border-b-primary md:border-b-0"
-          : ""
-      }`}
-      iconStyle="relative w-6 h-6 fill-primary"
-    />
+    <div className="relative">
+      {pathname === entry.fields.url && (
+        <motion.div
+          className="md:bg-background border-b-4 border-b-primary md:border-b-0 absolute top-0 left-0 w-full h-full z-0"
+          layoutId="nav-selected"
+          layout
+        />
+      )}
+      <AppLink
+        entry={entry}
+        className={`flex flex-col md:flex-row items-center gap-2 text-primary text-xl md:text-3xl md:pl-16 py-3 underline relative md:hover:shadow-xl border-transparent md:hover:border-primary border-t-2 border-b-2`}
+        iconStyle="relative w-6 h-6 fill-primary"
+      />
+    </div>
   );
 }
